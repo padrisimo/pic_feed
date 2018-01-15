8221943e4cfaf169d4a7b05702a9c16183daf377
padrisimo <nopuedoparardemolar@outlook.com>
2018-01-15 19:24:45 +0100

    passing reducers to store

---
+++ README.md
@@ -0,0 +1,16 @@
+# Reddit Feed
+
+Reddit Feed App
+
+### After cloning
+
+```
+> yarn
+> react-native run-android
+```
+
+*(or run-ios)
+
+### Misc
+
+Common Troubleshooting cases [here](http://facebook.github.io/react-native/docs/running-on-device.html)
--- src/app.js
+++ src/app.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
-import {  View, Text, } from 'react-native';
+import { View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './reducers';
 
-export default class App extends Component {
-  render() {
+const App = () => {
     return (
-      <View>
-        <Text> Reddit Feed </Text>
-      </View>
-    );
-  }
+        <Provider store={createStore(reducers)}>
+            <View />
+        </Provider>
+    )
 }
+
+export default App;
